07b25462885c29f99baa40b8503eac38ed2b2651
daonez <cyongchoi@gmail.com>
2022-03-02 00:51:44 +0900

    finished ws part

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,24 +1,42 @@
 const messageList = document.querySelector("ul")
-const messageForm = document.querySelector("form")
+const nickForm = document.querySelector("#nickname")
+const messageForm = document.querySelector("#message")
 const socket = new WebSocket(`ws://${window.location.host}`)
 
-socket.addEventListener("open", () => {
-  console.log("connected to Browser 😁")
-})
+function makeMessage(type, payload) {
+  const msg = { type, payload }
+  return JSON.stringify(msg)
+}
+
+function handleOpen() {
+  console.log("Connected to Server ✅")
+}
+
+socket.addEventListener("open", handleOpen)
 
 socket.addEventListener("message", (message) => {
-  console.log("message is:", message.data)
+  const li = document.createElement("li")
+  li.innerText = message.data
+  messageList.append(li)
 })
 
 socket.addEventListener("close", () => {
-  console.log("closed server 👾")
+  console.log("Disconnected from Server ❌")
 })
 
 function handleSubmit(event) {
   event.preventDefault()
   const input = messageForm.querySelector("input")
-  socket.send(input.value)
-  console.log(input.value)
+  socket.send(makeMessage("new_message", input.value))
   input.value = ""
 }
+
+function handleNickSubmit(event) {
+  event.preventDefault()
+  const input = nickForm.querySelector("input")
+  socket.send(makeMessage("nickname", input.value))
+  input.value = ""
+}
+
 messageForm.addEventListener("submit", handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
