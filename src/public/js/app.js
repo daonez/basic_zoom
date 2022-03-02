e30fc50bb127e7cfe8ad80f0437abc2a74c564a1
daonez <cyongchoi@gmail.com>
2022-03-02 19:17:16 +0900

    server가있으면 대처하는코드?

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,30 +15,44 @@ function addMessage(message) {
   ul.appendChild(li)
 }
 
-function showRoom() {
-  welcome.hidden = true
-  room.hidden = false
-  const h3 = room.querySelector("h3")
-  h3.innerText = `Room ${roomName}`
-  const form = room.querySelector("form")
-  form.addEventListener("submit", handleMessageSubmit)
-}
-
-function handleRoomSubmit(event) {
+function handleMessageSubmit(event) {
   event.preventDefault()
-  const input = form.querySelector("input")
+  const input = room.querySelector("#msg input")
+  const value = input.value
   socket.emit("new_message", input.value, roomName, () => {
     addMessage(`You: ${value}`)
   })
   input.value = ""
 }
+function handleNicknameSubmit(event) {
+  event.preventDefault()
+  const input = room.querySelector("#name input")
+  socket.emit("nickname", input.value)
+}
 
+function showRoom() {
+  welcome.hidden = true
+  room.hidden = false
+  const h3 = room.querySelector("h3")
+  h3.innerText = `Room ${roomName}`
+  const msgForm = room.querySelector("#msg")
+  const nameForm = room.querySelector("#name")
+  msgForm.addEventListener("submit", handleMessageSubmit)
+  nameForm.addEventListener("submit", handleNicknameSubmit)
+}
+function handleRoomSubmit(event) {
+  event.preventDefault()
+  const input = form.querySelector("input")
+  socket.emit("enter_room", input.value, showRoom)
+  roomName = input.value
+  input.value = ""
+}
 form.addEventListener("submit", handleRoomSubmit)
 
-socket.on("welcome", () => {
-  addMessage("someone joined")
+socket.on("welcome", (user) => {
+  addMessage(`${user} arrived!`)
 })
-socket.on("bye", () => {
-  addMessage("someone left")
+socket.on("bye", (left) => {
+  addMessage(`${left} left`)
 })
 socket.on("new_message", addMessage)
--- src/server.js
+++ src/server.js
@@ -15,22 +15,41 @@ app.get("/", (req, res) => {
 })
 app.get("/*", (req, res) => res.redirect("/"))
 
+// 데이타 베이스에 연결했을때 방이름을 알아내기위해서 사용
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = io
+  const publicRooms = []
+  rooms.forEach((value, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key)
+    }
+    console.log(publicRooms)
+    return publicRooms
+  })
+}
+
 io.on("connection", (socket) => {
+  socket["nickname"] = "Anon"
   socket.onAny((event) => {
     console.log(`Socket Event : ${event}`)
   })
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName)
     done()
-    socket.to(roomName).emit("welcome")
+    socket.to(roomName).emit("welcome", socket.nickname)
   })
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"))
+    socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname))
   })
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg)
+    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
     done()
   })
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname))
 })
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 server.listen(3000, handleListen)
