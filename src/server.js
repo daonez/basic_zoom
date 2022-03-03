67ad978bb739db130a1b9368957637696fef610d
daonez <cyongchoi@gmail.com>
2022-03-03 20:43:04 +0900

    simple function of camera

--- src/server.js
+++ src/server.js
@@ -1,76 +1,17 @@
-const http = require("http")
-const express = require("express")
+import http from "http"
+import SocketIO from "socket.io"
+import express from "express"
+
 const app = express()
-const { Server } = require("socket.io")
-const server = http.createServer(app)
 
-const { instrument } = require("@socket.io/admin-ui")
-
-const io = new Server(server, {
-  cors: {
-    origin: ["https://admin.socket.io"],
-    credentials: true,
-  },
-})
-
-instrument(io, {
-  auth: false,
-})
 app.set("view engine", "pug")
 app.set("views", __dirname + "/views")
-
 app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (_, res) => res.render("home"))
+app.get("/*", (_, res) => res.redirect("/"))
 
-app.get("/", (req, res) => {
-  res.render("home")
-})
-app.get("/*", (req, res) => res.redirect("/"))
+const httpServer = http.createServer(app)
+const wsServer = SocketIO(httpServer)
 
-// 데이타 베이스에 연결했을때 방이름을 알아내기위해서 사용
-
-function publicRooms() {
-  const {
-    sockets: {
-      adapter: { sids, rooms },
-    },
-  } = io
-  const publicRooms = []
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key)
-    }
-  })
-  return publicRooms
-}
-
-function countRoom(roomName) {
-  io.sockets.adapter.rooms.get(roomName)?.size
-}
-
-io.on("connection", (socket) => {
-  socket["nickname"] = "Anon"
-  socket.onAny((event) => {
-    console.log(`Socket Event : ${event}`)
-  })
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName)
-    done(countRoom(roomName))
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName))
-    io.sockets.emit("room_change", publicRooms())
-  })
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    )
-  })
-  socket.on("disconnect", () => {
-    io.sockets.emit("room_change", publicRooms())
-  })
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`)
-    done()
-  })
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname))
-})
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
-server.listen(3000, handleListen)
+httpServer.listen(3000, handleListen)
