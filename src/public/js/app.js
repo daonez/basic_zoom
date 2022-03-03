a3d06e567f87f087a795c6aaaa3e1471c1466a56
daonez <cyongchoi@gmail.com>
2022-03-03 21:45:45 +0900

    wip

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -4,19 +4,28 @@ const myFace = document.getElementById("myFace")
 const muteBtn = document.getElementById("mute")
 const cameraBtn = document.getElementById("camera")
 const camerasSelect = document.getElementById("cameras")
+const call = document.getElementById("call")
+
+call.hidden = true
 
 let myStream
 let muted = false
 let cameraOff = false
+let roomName
+let myPeerConnection
 
 async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices()
     const cameras = devices.filter((device) => device.kind === "videoinput")
+    const currentCamera = myStream.getVideoTracks()[0]
     cameras.forEach((camera) => {
       const option = document.createElement("option")
       option.value = camera.deviceId
       option.innerText = camera.label
+      if (currentCamera.label === camera.label) {
+        option.selected = true
+      }
       camerasSelect.appendChild(option)
     })
   } catch (e) {
@@ -24,20 +33,27 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  }
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  }
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      video: true,
-      audio: true,
-    })
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    )
     myFace.srcObject = myStream
-    await getCameras()
+    if (!deviceId) {
+      await getCameras()
+    }
   } catch (e) {
     console.log(e)
   }
 }
-
-getMedia()
 
 function handleMuteClick() {
   myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled))
@@ -60,5 +76,53 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value)
+}
+
 muteBtn.addEventListener("click", handleMuteClick)
 cameraBtn.addEventListener("click", handleCameraClick)
+camerasSelect.addEventListener("input", handleCameraChange)
+
+// Welcome Form (join a room)
+
+const welcome = document.getElementById("welcome")
+const welcomeForm = welcome.querySelector("form")
+
+async function startMedia() {
+  welcome.hidden = true
+  call.hidden = false
+  await getMedia()
+  makeConnection()
+}
+
+function handleWelcomeSubmit(event) {
+  event.preventDefault()
+  const input = welcomeForm.querySelector("input")
+  socket.emit("join_room", input.value, startMedia)
+  roomName = input.value
+  input.value = ""
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+// Socket Code
+
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer()
+  console.log(offer)
+  myPeerConnection.setLocalDescription(offer)
+  console.log("sent the offer")
+  socket.emit("offer", offer, roomName)
+})
+
+socket.on("offer", (offer) => {
+  console.log(offer)
+})
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection()
+  myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream))
+}
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import http from "http"
-import SocketIO from "socket.io"
-import express from "express"
+const http = require("http")
+const SocketIO = require("socket.io")
+const express = require("express")
 
 const app = express()
 
@@ -12,6 +12,17 @@ app.get("/*", (_, res) => res.redirect("/"))
 
 const httpServer = http.createServer(app)
 const wsServer = SocketIO(httpServer)
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName)
+    done()
+    socket.to(roomName).emit("welcome")
+  })
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer)
+  })
+})
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 httpServer.listen(3000, handleListen)
